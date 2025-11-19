5f405a643868e8313f0b4c90c5dee069cf774c36
js0.site <jssite@googlegroups.com>
2025-11-19 21:08:55 +0800

    .

--- ghbak/src/bak.js
+++ ghbak/src/bak.js
@@ -22,9 +22,11 @@ User root`,
   );
 
   $.verbose = 0;
-  await $`ssh -F ${ssh_config} -o ConnectTimeout=10 -o BatchMode=yes vps bash -c '"nix-shell -p openssl zstd gnutar --run \\"export PASSWORD=${PASSWORD} && set -ex && cd ${dir} && tar --remove-files -C backup -cf - . | zstd -18 -T0 | openssl enc -aes-256-cbc -pbkdf2 -salt -pass env:PASSWORD -out ${fname} && set +x\\""'`.pipe(
-    process.stdout,
-  );
+  const p =
+    $`ssh -F ${ssh_config} -o ConnectTimeout=10 -o BatchMode=yes vps bash -c '"nix-shell -p openssl zstd gnutar --run \\"export PASSWORD=${PASSWORD} && set -ex && cd ${dir} && tar --remove-files -C backup -cf - . | zstd -18 -T0 | openssl enc -aes-256-cbc -pbkdf2 -salt -pass env:PASSWORD -out ${fname} && set +x\\""'`.pipe(
+      process.stdout,
+    );
   $.verbose = 1;
+  await p;
   await $`rsync --remove-source-files -e 'ssh -F ${ssh_config}' -avz vps:${fname} ${fname} && gh release upload ${today} ${fname}`;
 };
