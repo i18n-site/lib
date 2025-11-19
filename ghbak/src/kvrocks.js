e3e51c70532b253e99f51608dcab4d0e1ad38cf6
js0.site <jssite@googlegroups.com>
2025-11-19 21:04:39 +0800

    .

--- ghbak/src/kvrocks.js
+++ ghbak/src/kvrocks.js
@@ -1,18 +1,15 @@
 #!/usr/bin/env bun
 
-import Redis from "@3-/ioredis";
-import int from "@3-/int";
-import sleep from "@3-/sleep";
 import { $ } from "zx";
 import write from "@3-/write";
 import ymd from "@3-/ymd";
 
 const { PASSWORD } = process.env;
 
-export default async (name, ip) => {
+export default async (name, ip, dir) => {
   const ssh_config = `/tmp/ssh_config/${name}`,
     fname = `/tmp/${name}.tar.zst.enc`,
-    today = new Date().toISOString().slice(0, 10);
+    today = ymd();
   write(
     ssh_config,
     `
@@ -25,7 +22,7 @@ User root`,
   );
 
   $.verbose = 0;
-  await $`ssh -F ${ssh_config} -o ConnectTimeout=10 -o BatchMode=yes kvrocks bash -c '"nix-shell -p openssl zstd gnutar --run \\"export PASSWORD=${PASSWORD} && set -ex && cd /var/lib/kvrocks && tar --remove-files -C backup -cf - . | zstd -18 -T0 | openssl enc -aes-256-cbc -pbkdf2 -salt -pass env:PASSWORD -out ${fname} && set +x\\""'`.pipe(
+  await $`ssh -F ${ssh_config} -o ConnectTimeout=10 -o BatchMode=yes kvrocks bash -c '"nix-shell -p openssl zstd gnutar --run \\"export PASSWORD=${PASSWORD} && set -ex && cd ${dir} && tar --remove-files -C backup -cf - . | zstd -18 -T0 | openssl enc -aes-256-cbc -pbkdf2 -salt -pass env:PASSWORD -out ${fname} && set +x\\""'`.pipe(
     process.stdout,
   );
   $.verbose = 1;
