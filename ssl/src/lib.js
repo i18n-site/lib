bc83867440ffc79dd31e0c317b05cbccf8432026
js0.site <jssite@googlegroups.com>
2025-11-20 15:04:53 +0800

    .

--- ssl/src/lib.js
+++ ssl/src/lib.js
@@ -1,22 +1,73 @@
+import { Client, forge } from "acme-client";
+import { generateKeyPair } from "crypto";
+import { promisify } from "util";
 import sleep from "@3-/sleep";
 
-export default async (
-  host,
-  setCname, // prefix value
-  rmById,
-) => {
-  const cname = await 申请证书(host);
+const _dir = import.meta.dirname;
+const generate_key_pair = promisify(generateKeyPair);
 
-  const id = await setCname(cname),
-    waited = 0;
-  try {
-    while (waited < 120) {
-      // TODO 等待cname生效
-      await sleep(1e3);
+const 生成密钥 = async () => {
+  const { privateKey } = await generate_key_pair("ec", {
+    namedCurve: "P-384",
+    publicKeyEncoding: { type: "spki", format: "pem" },
+    privateKeyEncoding: { type: "pkcs8", format: "pem" },
+  });
+  return privateKey;
+};
+
+const 生成请求 = async (key_pem, domains) => {
+  const private_key = forge.pki.privateKeyFromPem(key_pem);
+  const [common_name, ...alt_names] = domains;
+  const [_, csr] = await forge.createCsr({
+    key: private_key,
+    commonName: common_name,
+    altNames: alt_names,
+  });
+  return csr;
+};
+
+export default async (host, set_cname, rm_by_id) => {
+  const account_key = await 生成密钥();
+  const client = new Client({
+    directoryUrl: "https://acme.zerossl.com/v2/DV90",
+    accountKey: account_key,
+  });
+
+  await client.createAccount({
+    termsOfServiceAgreed: true,
+    contact: ["mailto:i18n.site@gmail.com"],
+  });
+
+  const certificate_key = await 生成密钥();
+  const domains = [host, `*.${host}`];
+  const csr = await 生成请求(certificate_key, domains);
+
+  const challenge_ids = new Map();
+
+  const create_challenge = async (authz, challenge, key_authorization) => {
+    if (challenge.type === "dns-01") {
+      const id = await set_cname(key_authorization);
+      challenge_ids.set(challenge.token, id);
+      await sleep(10000);
     }
-    // TODO 获取证书
-    return [key, crt];
-  } finally {
-    await rmById(id);
-  }
+  };
+
+  const remove_challenge = async (authz, challenge) => {
+    const id = challenge_ids.get(challenge.token);
+    if (id) {
+      await rm_by_id(id);
+      challenge_ids.delete(challenge.token);
+    }
+  };
+
+  const certificate = await client.auto({
+    csr,
+    email: "i18n.site@gmail.com",
+    termsOfServiceAgreed: true,
+    challengePriority: ["dns-01"],
+    challengeCreateFn: create_challenge,
+    challengeRemoveFn: remove_challenge,
+  });
+
+  return [certificate_key, certificate];
 };
