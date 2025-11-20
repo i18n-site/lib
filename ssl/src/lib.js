b3bf297702390a7161e61e65be7226681eeb9ae6
js0.site <jssite@googlegroups.com>
2025-11-20 16:32:42 +0800

    .

--- ssl/src/lib.js
+++ ssl/src/lib.js
@@ -1,5 +1,7 @@
 import { Client, crypto } from "acme-client";
 
+const ACME_CHALLENGE = "_acme-challenge";
+
 export default async (domain, setTxt, rmTxt) => {
   const email = "ssl@" + domain,
     client = new Client({
@@ -19,10 +21,10 @@ export default async (domain, setTxt, rmTxt) => {
     challengePriority: ["dns-01"],
     challengeCreateFn: async (authz, challenge, key_auth) => {
       console.log({ authz, challenge, key_auth });
-      await setTxt("_acme-challenge", '"' + key_auth + '"');
+      await setTxt(ACME_CHALLENGE, '"' + key_auth + '"');
     },
     challengeRemoveFn: async (authz, challenge, key_auth) => {
-      await rmTxt("_acme-challenge");
+      await rmTxt(ACME_CHALLENGE);
     },
   });
 
