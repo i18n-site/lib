8ba63526e7c6d4c23b1977155ea9f60446c51caa
js0.site <jssite@googlegroups.com>
2025-11-20 15:45:29 +0800

    .

--- ssl/src/lib.js
+++ ssl/src/lib.js
@@ -4,13 +4,13 @@ import sleep from "@3-/sleep";
 export default async (domain, setTxt, rmTxtById) => {
   const domain_li = ["*." + domain, domain],
     challenge = await "TODO",
-    cname_id = await setTxt("xxx." + domain, challenge);
+    txt_id = await setTxt("xxx." + domain, challenge);
 
   try {
     await sleep(6e3);
     const [key, cert] = "TODO";
     return [key, cert];
   } finally {
-    await rmTxtById(cname_id);
+    await rmTxtById(txt_id);
   }
 };
