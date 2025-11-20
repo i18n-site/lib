6a72d1fbab628c2413f50b2cb730acbdf5e5fb12
js0.site <jssite@googlegroups.com>
2025-11-20 14:23:02 +0800

    .

--- cf/src/zone.js
+++ cf/src/zone.js
@@ -2,16 +2,21 @@ export default async ({ DELETE, GET, POST }, host) => {
   const zone_id = (await GET("zones?name=" + host))[0].id,
     getName = (name) => (name ? name + "." + host : host),
     prefix = `zones/${zone_id}/`,
+    dns_records = prefix + "dns_records",
     set = async (type, name, content, proxied) => {
       return (
-        await POST(prefix + "dns_records", {
+        await POST(dns_records, {
           type,
           name: getName(name),
           content,
           proxied: !!proxied,
         })
       ).id;
-    };
+    },
+    idByName = async (type, name) =>
+      (await GET(dns_records + `?name=${getName(name)}&type=${type}`)).id,
+    rmByName = async (type, name) =>
+      DELETE(dns_records + "/" + (await idByName(type, name)));
   return {
     set: async (type, name, content, proxied = false) => {
       let n = 0;
@@ -19,12 +24,12 @@ export default async ({ DELETE, GET, POST }, host) => {
         try {
           return await set(type, name, content, proxied);
         } catch (e) {
-          if (++n > 1 || !e.message?.includes("already exists")) {
+          if (++n > 1 || !e.message?.includes(" already exists")) {
             throw e;
           }
+          console.log(await rmByName(type, name));
         }
       }
     },
-    rm: (type, name) => DELETE(prefix),
   };
 };
