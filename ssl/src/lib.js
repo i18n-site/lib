23c158db2a67035d62a7d99f94a371d0ac0b9546
js0.site <jssite@googlegroups.com>
2025-11-20 16:30:57 +0800

    .

--- ssl/src/lib.js
+++ ssl/src/lib.js
@@ -1,13 +1,5 @@
 import { Client, crypto } from "acme-client";
 
-const 创建客户端 = async (email) => {
-  const account_key = await crypto.createPrivateKey();
-  return new Client({
-    directoryUrl: "https://acme-v02.api.letsencrypt.org/directory",
-    accountKey: account_key,
-  });
-};
-
 const 清理挑战 = async (authz, challenge, key_auth, rmTxtById, txt_id_map) => {
   if (challenge.type === "dns-01") {
     const id = txt_id_map.get(authz.identifier.value);
@@ -19,7 +11,11 @@ const 清理挑战 = async (authz, challenge, key_auth, rmTxtById, txt_id_map) =
 };
 
 export default async (domain, setTxt, rmTxt) => {
-  const client = await 创建客户端('ssl@'+domain);
+  const email = "ssl@" + domain,
+    client = new Client({
+      directoryUrl: "https://acme-v02.api.letsencrypt.org/directory",
+      accountKey: await crypto.createPrivateKey(),
+    });
 
   const [cert_key, csr] = await crypto.createCsr({
     commonName: domain,
@@ -32,7 +28,7 @@ export default async (domain, setTxt, rmTxt) => {
     termsOfServiceAgreed: true,
     challengePriority: ["dns-01"],
     challengeCreateFn: async (authz, challenge, key_auth) => {
-      console.log({ authz, challenge key_auth});
+      console.log({ authz, challenge, key_auth });
       await setTxt("_acme-challenge", '"' + key_auth + '"');
     },
     challengeRemoveFn: async (authz, challenge, key_auth) => {
