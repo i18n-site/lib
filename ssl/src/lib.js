0d9fc29f7afbc0c0e71c8d76852fdc7c12fc966a
js0.site <jssite@googlegroups.com>
2025-11-20 15:49:19 +0800

    .

--- ssl/src/lib.js
+++ ssl/src/lib.js
@@ -60,14 +60,15 @@ const 清理挑战 = async (authz, challenge, key_auth, rmTxtById, txt_id_map) =
 
 export default async (domain, setTxt, rmTxtById) => {
   const domain_li = ["*." + domain, domain];
-  const client = await 创建客户端();
+  const email = "ssl@" + domain;
+  const client = await 创建客户端(email);
   const txt_id_map = new Map();
 
   const [cert_key, csr] = await 生成证书签名请求(domain_li);
 
   const cert = await client.auto({
     csr,
-    email: "ssl@" + domain,
+    email,
     termsOfServiceAgreed: true,
     challengePriority: ["dns-01"],
     challengeCreateFn: async (authz, challenge, key_auth) => {
