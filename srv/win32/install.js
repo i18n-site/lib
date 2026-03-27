22da8cf00879d05ec62811eeba00d34277d3797a
js0.site <jssite@googlegroups.com>
2026-03-27 15:03:54 +0800

    fix(win32): implement ONSTART boot rule and explicitly use nodejs engine

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -3,12 +3,11 @@ import { $ } from "zx";
 process.env.MSYS_NO_PATHCONV = "1";
 
 export default async ({ name, scriptPath: script_path }) => {
-  const { execPath: exec_path } = process;
   try {
     await $`schtasks /End /TN ${name}`;
   } catch (e) {}
 
-  const cmd = `schtasks /Create /F /TN ${name} /TR "\\"${exec_path}\\" \\"${script_path}\\" run" /SC ONLOGON /RL HIGHEST`;
+  const cmd = `schtasks /Create /F /TN ${name} /TR "node \\"${script_path}\\" run" /SC ONSTART /RL HIGHEST`;
   await $([cmd]);
 
   await $`schtasks /Run /TN ${name}`;
