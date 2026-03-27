3a1ed7bcbd2371665207ef92be2e9bc777fa45b5
js0.site <jssite@googlegroups.com>
2026-03-27 16:17:42 +0800

    .

--- srv/test_gen.js
+++
@@ -1,6 +0,0 @@
-import gen from "@3-/obj_replace/gen.js";
-gen(
-  { name: "test_srv", execPath: "/usr/bin/node", scriptPath: "/home/z/中 文 目 录 测试/dummy.js" },
-  "linux/systemd.service",
-  "test_systemd.service"
-);
