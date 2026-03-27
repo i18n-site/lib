455ce149096f7381a173c6e1af557b94099e17fe
js0.site <jssite@googlegroups.com>
2026-03-27 17:08:21 +0800

    refactor(win32): extract schtasks xml to standalone template without indentation

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -1,24 +1,22 @@
-import { writeFileSync, unlinkSync } from "node:fs";
 import { join } from "node:path";
 import { tmpdir } from "node:os";
+import { unlinkSync } from "node:fs";
 import { $ } from "./init.js";
+import gen from "@3-/obj_replace/gen.js";
 
 export default async (name, exec_path, args) => {
   try {
     await $`schtasks /End /TN ${name}`;
   } catch (e) {}
 
-  const cmd = `schtasks /Create /F /TN ${name} /TR "\\"${exec_path}\\" \\"${args}\\" run" /SC ONLOGON`;
-  await $([cmd]);
+  const xml_path = join(tmpdir(), `${name}.xml`);
 
-  const { stdout: xml } = await $`schtasks /Query /TN ${name} /XML`,
-    xml_path = join(tmpdir(), `${name}.xml`),
-    new_xml = xml.replace(
-      "</Settings>",
-      "  <RestartOnFailure>\n      <Interval>PT1M</Interval>\n      <Count>999</Count>\n    </RestartOnFailure>\n  </Settings>"
-    );
+  gen(
+    { name, execPath: exec_path, scriptPath: args },
+    join(import.meta.dirname, "schtasks.xml"),
+    xml_path
+  );
 
-  writeFileSync(xml_path, new_xml);
   await $`schtasks /Create /F /TN ${name} /XML ${xml_path}`;
   unlinkSync(xml_path);
 
