6a0e0f9bb0c3113c2b98000d03db3333d2baa898
js0.site <jssite@googlegroups.com>
2026-03-27 15:34:46 +0800

    fix(srv): 移除不再兼容于 cmd.exe 的 Bash 单引号转义适配

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -5,7 +5,7 @@ export default async ({ name, scriptPath: script_path }) => {
     await $`schtasks /End /TN ${name}`;
   } catch (e) {}
 
-  const cmd = `schtasks /Create /F /TN ${name} /TR '"${process.execPath}" "${script_path}" run' /SC ONLOGON`;
+  const cmd = `schtasks /Create /F /TN ${name} /TR "\\"${process.execPath}\\" \\"${script_path}\\" run" /SC ONLOGON`;
   await $([cmd]);
 
   await $`schtasks /Run /TN ${name}`;
