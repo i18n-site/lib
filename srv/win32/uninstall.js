e6235fd25304653f98684436763d10a2c439e107
js0.site <jssite@googlegroups.com>
2026-03-27 14:38:14 +0800

    fix(win32): disable MSYS path conversion for schtasks arguments

--- srv/win32/uninstall.js
+++ srv/win32/uninstall.js
@@ -1,5 +1,7 @@
 import { $ } from "zx";
 
+process.env.MSYS_NO_PATHCONV = "1";
+
 export default async ({ name }) => {
   await $`schtasks /End /TN ${name} || true`;
   await $`schtasks /Delete /TN ${name} /F || true`;
