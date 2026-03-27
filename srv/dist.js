15fd131bf66e98798eaa22194680cd25922ac9c1
js0.site <jssite@googlegroups.com>
2026-03-27 15:48:13 +0800

    feat(srv): 基于内容 hash 实现增量发布 / Implement hash-based incremental publishing

--- srv/dist.js
+++ srv/dist.js
@@ -1,6 +1,85 @@
 #!/usr/bin/env bun
 
-import { $ } from 'zx'
+import { $, cd } from 'zx'
+import { join } from 'path'
+import { readFileSync, writeFileSync, existsSync } from 'fs'
+import yaml from 'js-yaml'
+import packlist from 'npm-packlist'
+import Arborist from '@npmcli/arborist'
 
-await $`bunx changeset version`
-await $`bunx changeset publish`
+$.verbose = 1
+
+const
+  dir = import.meta.dirname,
+  hash_file = join(dir, 'publish.yml'),
+  packages = ['darwin', 'linux', 'win32', 'srv'],
+  hashes = existsSync(hash_file) ? (yaml.load(readFileSync(hash_file, 'utf8')) || {}) : {},
+  getHash = async (data) => Buffer.from(await crypto.subtle.digest("SHA-256", data)).toString('base64url'),
+  changed = []
+
+for (const pkg of packages) {
+  const
+    pkg_dir = join(dir, pkg),
+    arborist = new Arborist({ path: pkg_dir })
+
+  cd(pkg_dir)
+
+  const
+    tree = await arborist.loadActual(),
+    files = await packlist(tree),
+    buffers = []
+
+  files.sort()
+
+  for (const file of files) {
+    const
+      file_path = join(pkg_dir, file),
+      is_pkg_json = file === 'package.json'
+
+    buffers.push(new TextEncoder().encode(file))
+
+    if (is_pkg_json) {
+      const { version, ...rest } = JSON.parse(readFileSync(file_path, 'utf8'))
+      buffers.push(new TextEncoder().encode(JSON.stringify(rest)))
+    } else {
+      buffers.push(new Uint8Array(readFileSync(file_path)))
+    }
+  }
+
+  const
+    total_len = buffers.reduce((acc, b) => acc + b.length, 0),
+    merged = new Uint8Array(total_len)
+
+  let offset = 0
+  for (const b of buffers) {
+    merged.set(b, offset)
+    offset += b.length
+  }
+
+  const current_hash = await getHash(merged)
+
+  if (hashes[pkg] !== current_hash) {
+    const { name } = JSON.parse(readFileSync(join(pkg_dir, 'package.json'), 'utf8'))
+    changed.push({
+      dir: pkg,
+      name,
+      hash: current_hash
+    })
+  }
+}
+
+if (changed.length > 0) {
+  cd(dir)
+
+  const changeset_content = `---\n${changed.map(({ name }) => `"${name}": patch`).join('\n')}\n---\n\nauto update\n`
+
+  writeFileSync(join(dir, '.changeset', 'auto-update.md'), changeset_content, 'utf8')
+
+  await $`bunx changeset version`
+  await $`bunx changeset publish`
+
+  for (const { dir: pkg_dir, hash } of changed) {
+    hashes[pkg_dir] = hash
+  }
+  writeFileSync(hash_file, yaml.dump(hashes), 'utf8')
+}
--- srv/win32/CHANGELOG.md
+++ srv/win32/CHANGELOG.md
@@ -1,5 +1,23 @@
 # @3-/srv-win32
 
+## 0.1.13
+
+### Patch Changes
+
+- auto update
+
+## 0.1.12
+
+### Patch Changes
+
+- auto update
+
+## 0.1.11
+
+### Patch Changes
+
+- auto update
+
 ## 0.1.10
 
 ### Patch Changes
