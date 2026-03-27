251871480d18a594b87f231ef11d121a3e19b92e
js0.site <jssite@googlegroups.com>
2026-03-27 17:26:52 +0800

    fix(win32): import fs readFileSync and writeFileSync correctly

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -1,6 +1,6 @@
 import { join } from "node:path";
 import { tmpdir } from "node:os";
-import { unlinkSync } from "node:fs";
+import { unlinkSync, readFileSync, writeFileSync } from "node:fs";
 import { $ } from "./init.js";
 import gen from "@3-/obj_replace/gen.js";
 
@@ -16,6 +16,9 @@ export default async (name, exec_path, args) => {
     join(import.meta.dirname, "schtasks.xml"),
     xml_path
   );
+
+  const xml_str = readFileSync(xml_path, "utf8");
+  writeFileSync(xml_path, "\uFEFF" + xml_str, "utf16le");
 
   await $([`schtasks /Create /F /TN ${name} /XML "${xml_path}"`]);
   unlinkSync(xml_path);
