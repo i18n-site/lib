eb68d697cd818e68d100548e0b8305672decbe74
js0.site <jssite@googlegroups.com>
2026-03-27 14:40:44 +0800

    fix(win32): bypass zx quoting for schtasks /TR argument

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -4,7 +4,12 @@ process.env.MSYS_NO_PATHCONV = "1";
 
 export default async ({ name, scriptPath: script_path }) => {
   const { execPath: exec_path } = process;
-  await $`schtasks /End /TN ${name} || true`;
-  await $`schtasks /Create /F /TN ${name} /TR ${`"${exec_path}" "${script_path}" run`} /SC ONLOGON /RL HIGHEST`;
+  try {
+    await $`schtasks /End /TN ${name}`;
+  } catch (e) {}
+
+  const cmd = `schtasks /Create /F /TN ${name} /TR "\\"${exec_path}\\" \\"${script_path}\\" run" /SC ONLOGON /RL HIGHEST`;
+  await $([cmd]);
+
   await $`schtasks /Run /TN ${name}`;
 };
