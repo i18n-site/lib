246284b2661cb6bcc97d6f030d80374f2e3f37d7
js0.site <jssite@googlegroups.com>
2026-03-27 17:06:40 +0800

    feat: configure 1 minute restart interval on failure for all platform services

--- srv/win32/install.js
+++ srv/win32/install.js
@@ -1,3 +1,6 @@
+import { writeFileSync, unlinkSync } from "node:fs";
+import { join } from "node:path";
+import { tmpdir } from "node:os";
 import { $ } from "./init.js";
 
 export default async (name, exec_path, args) => {
@@ -7,6 +10,17 @@ export default async (name, exec_path, args) => {
 
   const cmd = `schtasks /Create /F /TN ${name} /TR "\\"${exec_path}\\" \\"${args}\\" run" /SC ONLOGON`;
   await $([cmd]);
+
+  const { stdout: xml } = await $`schtasks /Query /TN ${name} /XML`,
+    xml_path = join(tmpdir(), `${name}.xml`),
+    new_xml = xml.replace(
+      "</Settings>",
+      "  <RestartOnFailure>\n      <Interval>PT1M</Interval>\n      <Count>999</Count>\n    </RestartOnFailure>\n  </Settings>"
+    );
+
+  writeFileSync(xml_path, new_xml);
+  await $`schtasks /Create /F /TN ${name} /XML ${xml_path}`;
+  unlinkSync(xml_path);
 
   await $`schtasks /Run /TN ${name}`;
 };
