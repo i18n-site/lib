9311f0ea15066e335ff20763e57529b398e08e67
js0.site <jssite@googlegroups.com>
2026-03-28 06:48:33 +0800

    @3-/base64url v0.1.2

--- base64url/tests/main.test.js
+++ base64url/tests/main.test.js
@@ -2,6 +2,8 @@
 import { test } from "vitest";
 import bufB64 from "../src/bufB64.js";
 import uint8B64 from "../src/uint8B64.js";
+import b64Buf from "../src/b64Buf.js";
+import b64Uint8 from "../src/b64Uint8.js";
 
 const cases = [
   ["", ""],
@@ -23,5 +25,18 @@ for (const [input, expected] of cases) {
   test(`uint8B64: ${JSON.stringify(input)}`, () => {
     const uint8 = new TextEncoder().encode(input);
     uint8B64(uint8) === expected || console.error(`Expected: ${expected}, got: ${uint8B64(uint8)}`);
+  });
+
+  test(`b64Buf: ${JSON.stringify(input)}`, () => {
+    const buf = Buffer.from(input),
+      decoded = b64Buf(expected);
+    buf.equals(decoded) || console.error(`Expected: ${input}, got: ${decoded}`);
+  });
+
+  test(`b64Uint8: ${JSON.stringify(input)}`, () => {
+    const uint8 = new TextEncoder().encode(input),
+      decoded = b64Uint8(expected),
+      equal = uint8.length === decoded.length && uint8.every((v, i) => v === decoded[i]);
+    equal || console.error(`Expected: ${input}, got: ${new TextDecoder().decode(decoded)}`);
   });
 }
