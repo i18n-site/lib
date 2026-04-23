e60d7a9c3ef05d81a23c400a4db1eb01d0c3b64b
js0.site <jssite@googlegroups.com>
2026-04-23 12:00:54 +0800

    @3-/lang v0.2.12

--- lang/src/code/QWEN.js
+++ lang/src/code/QWEN.js
@@ -1,3 +1,14 @@
 export default {
   no: "nn",
+  "zh-TW": "zh_tw",
 };
+
+export const MISS = new Set([
+  "ga", // 爱尔兰语
+  "su", // 巽他语
+  "pa", // 旁遮普语
+  "ml", // 马拉雅拉姆语
+  "ht", // 海地克里奥尔语
+  "tg", // 塔吉克语
+  "yi", // 意第绪语
+]);
