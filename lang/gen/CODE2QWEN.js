d3ce212acf91d77a9847f571a52e1ff9e1da09ff
js0.site <jssite@googlegroups.com>
2026-04-23 12:48:42 +0800

    .

---
+++ lang/gen/CODE2QWEN.js
@@ -0,0 +1,5 @@
+export default {
+  iw: "he", // 希伯来语
+  fil: "tl", // 菲律宾语
+  no: "nb", // 挪威语
+};
--- lang/gen/gen.js
+++ lang/gen/gen.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env bun
-
 import gemini from "./gemini.js";
 import qwen from "./qwen3Mt.js";
+import CODE2QWEN from "./CODE2QWEN.js";
 import Table from "cli-table3";
 
 const g_map = new Map(gemini.map(([name, code]) => [code, name])),
   q_map = new Map(qwen.map(([en, zh, code]) => [code, [zh, en]])),
-  all_codes = new Set([...g_map.keys(), ...q_map.keys()]),
+  all_codes = new Set([
+    ...g_map.keys(),
+    ...q_map.keys(),
+    ...Object.values(CODE2QWEN),
+  ]),
   common = [],
   missing_qwen = [],
   missing_gemini = [],
@@ -32,23 +36,46 @@ const g_map = new Map(gemini.map(([name, code]) => [code, name])),
       style: { "padding-left": 0, "padding-right": 0 },
     });
 
+const seen_q = new Set();
+const seen_g = new Set();
+
 for (const code of all_codes) {
-  const g_name = g_map.get(code),
-    q_info = q_map.get(code);
+  let g_code = code,
+    q_code = code;
+
+  for (const [g, q] of Object.entries(CODE2QWEN)) {
+    if (code === g || code === q) {
+      g_code = g;
+      q_code = q;
+      break;
+    }
+  }
+
+  const g_name = g_map.get(g_code),
+    q_info = q_map.get(q_code);
 
   if (g_name && q_info) {
     const [q_zh, q_en] = q_info;
-    common.push([code, g_name, q_zh, q_en]);
-  } else if (g_name) {
-    missing_qwen.push([code, g_name]);
-  } else if (q_info) {
-    const [q_zh, q_en] = q_info;
-    missing_gemini.push([code, q_zh, q_en]);
+    common.push([`${g_code}/${q_code}`, g_name, q_zh, q_en]);
+    seen_g.add(g_code);
+    seen_q.add(q_code);
+  }
+}
+
+for (const [code, name] of g_map) {
+  if (!seen_g.has(code)) {
+    missing_qwen.push([code, name]);
+  }
+}
+
+for (const [code, info] of q_map) {
+  if (!seen_q.has(code)) {
+    missing_gemini.push([code, ...info]);
   }
 }
 
 const log = (title, data) => {
-  console.log("\n=== " + title + " ===");
+  console.log(`\n=== ${title} (${data.length}) ===`);
   const t = new_table();
   t.push(...data);
   console.log(t.toString());
