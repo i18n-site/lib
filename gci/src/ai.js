5ec83dc39f1a699da634e5ea7f002b266471c35c
js0.site <jssite@googlegroups.com>
2026-05-30 06:25:59 +0800

    @3-/gci v0.1.6

--- gci/src/ai.js
+++ gci/src/ai.js
@@ -47,7 +47,7 @@ export default async (diff_text) => {
   return (
     await ai(
       (process.env.GCI_PROMPT ||
-        "根据以下代码改动，生成一句话的git提交消息，格式如`type: 英文说明\n类型: 中文说明`。不要返回其他多余的说明，仅返回提交消息即可。") +
+        "根据以下代码改动，生成一句话的git提交消息，格式如`type: 英文说明\n类型: 中文说明`。这里『类型』，是type的中文翻译，不要直接写『类型』。不要返回其他多余的说明，仅返回提交消息即可。") +
         "\n\n" +
         diff_text,
     )
