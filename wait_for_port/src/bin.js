ec543af6451cc3f07119335fc5fb76349ada21ca
js0.site <jssite@googlegroups.com>
2026-05-31 03:01:09 +0800

    @3-/wait_for_port v0.1.2

--- wait_for_port/src/bin.js
+++ wait_for_port/src/bin.js
@@ -1,10 +1,9 @@
-#! /usr/bin/env bun
-
-import { Socket } from "node:net";
+#!/usr/bin/env bun
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import { $ } from "zx";
 import sleep from "@3-/sleep";
+import checkPort from "./lib.js";
 
 const argv = yargs(hideBin(process.argv))
     .usage("用法: $0 <端口或网址> [命令]")
@@ -12,31 +11,14 @@ const argv = yargs(hideBin(process.argv))
     .parse(),
   [url, cmd] = argv._,
   li = String(url).split(":"),
-  [host, port] = li.length > 1 ? [li[0], +li[1]] : ["127.0.0.1", +li[0]],
-  checkPort = (h, p) =>
-    new Promise((resolve) => {
-      const sock = new Socket(),
-        finish = (status) => {
-          sock.destroy();
-          resolve(status);
-        };
+  [host, port] = li.length > 1 ? [li[0], +li[1]] : ["127.0.0.1", +li[0]];
 
-      sock.setTimeout(1000);
-      sock.on("connect", () => finish(true));
-      sock.on("timeout", () => finish(false));
-      sock.on("error", () => finish(false));
-      sock.connect(p, h);
-    }),
-  waitPort = async () => {
-    if (!(await checkPort(host, port))) {
-      if (cmd) {
-        await $`sh -c ${cmd}`;
-      }
-      while (!(await checkPort(host, port))) {
-        await sleep(1000);
-      }
-    }
-  };
-
-await waitPort();
+if (!(await checkPort(host, port))) {
+  if (cmd) {
+    await $(["sh -c " + cmd]);
+  }
+  while (!(await checkPort(host, port))) {
+    await sleep(1000);
+  }
+}
 process.exit();
