19d784650ba8548646589108ca9df47b0180d65f
js0.site <jssite@googlegroups.com>
2026-06-09 13:29:04 +0800

    @3-/tcpping v0.1.9

--- tcpping/src/ping.js
+++ tcpping/src/ping.js
@@ -2,7 +2,8 @@ import ping from "./lib.js";
 
 export default async (host, port, timeout) => {
   try {
-    await ping(host, port, timeout);
-    return 1;
+    if (await ping(host, port, timeout)) {
+      return 1;
+    }
   } catch {}
 };
