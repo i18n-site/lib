f94e30ca7a4a998682a00e942668b01021fa36a8
js0.site <jssite@googlegroups.com>
2026-09-14 11:30:45 +0800

    refactor: 重构push逻辑，新增dev分支推送main

--- gci/src/git/push.js
+++ gci/src/git/push.js
@@ -1,10 +1,36 @@
-export default async (git, branch, from, to) => {
-  const target_li = from && to ? [from + ":" + to] : [],
-    pushed = await git.push("origin", ...target_li).catch(() => null);
-  if (!pushed) {
-    const dest = to || branch;
-    await git.fetch("origin", dest).catch(() => null);
-    await git.merge(["--ff", "--no-edit", "origin/" + dest]).catch(() => null);
-    await git.push("origin", ...target_li).catch(() => null);
+const singlePush = async (git, branch, from, to) => {
+    const target_li = from && to ? [from + ":" + to] : [branch],
+      pushed = await git.push("origin", ...target_li).catch(() => null);
+    if (!pushed) {
+      const dest = to || branch;
+      await git.fetch("origin", dest).catch(() => null);
+      await git
+        .merge(["--ff", "--no-edit", "origin/" + dest])
+        .catch(() => null);
+      await git.push("origin", ...target_li).catch(() => null);
+    }
+  },
+  devPush = async (git) => {
+    await singlePush(git, "dev");
+
+    const pushed_main = await git
+      .push("origin", "dev:main")
+      .catch(() => null);
+
+    if (!pushed_main) {
+      await git.fetch("origin", "main").catch(() => null);
+      await git.merge(["--no-edit", "origin/main"]).catch(() => null);
+      await git.push("origin", "dev").catch(() => null);
+      await git.push("origin", "dev:main").catch(() => null);
+    }
+
+    await git.branch(["-f", "main", "dev"]).catch(() => null);
+  };
+
+export default async (git, branch) => {
+  if (branch === "dev") {
+    await devPush(git);
+  } else {
+    await singlePush(git, branch);
   }
 };
